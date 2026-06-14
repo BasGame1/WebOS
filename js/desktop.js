397c066f3ef4456e111714a20fb49c3936cc15de
BasGame1 <abadbasantaalex@gmail.com>
2026-06-14 18:09:43 +0200

    js: desktop: Fix javascript for the change from terminal to window

--- js/desktop.js
+++ js/desktop.js
@@ -5,13 +5,29 @@ const window_screen = document.getElementById('window')
 
 function ChangeToWindow(e) {
     e.preventDefault();
+    // Remove classes
+    button_window.classList.remove("hide_current");
+    window_screen.classList.remove("hide_current");
+    terminal_screen.classList.remove("show_hidden");
+    button_terminal.classList.remove("show_hidden");
+    //Add classes
     terminal_screen.classList.add("hide_current");
     window_screen.classList.add("show_hidden");
-}
+    button_terminal.classList.add("hide_current");
+    button_window.classList.add("show_hidden");
+}   
 function ChangeToTerminal(e) {
     e.preventDefault();
+    // remove classses
+    button_terminal.classList.remove("hide_current");
+    terminal_screen.classList.remove("hide_current");
+    window_screen.classList.remove("show_hidden");
+    button_window.classList.remove("show_hidden")
+    // Add classes
     window_screen.classList.add("hide_current");
     terminal_screen.classList.add("show_hidden");
+    button_window.classList.add("hide_current");
+    button_terminal.classList.add("show_hidden");
 }
 button_terminal.addEventListener('click', ChangeToWindow, false);
 button_window.addEventListener('click', ChangeToTerminal, false);
